285f3fe93451f88e1e8810a392f97edae4d74da8
Dzmitry Karpuk <dzmitry.karpuk@corp.itibo.com>
2024-11-17 16:16:05 +0300

    added utf-8

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,11 +1,13 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BasePage {
-    protected static WebDriver driver;
+    //protected static WebDriver driver;
+    protected static RemoteWebDriver driver;
 
-    public static void setDriver(WebDriver webDriver){
+    public static void setDriver(RemoteWebDriver webDriver){
         driver = webDriver;
     }
 
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,17 +5,28 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import pages.BasePage;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 abstract public class TestBase {
-    protected static WebDriver driver;
+    //protected static WebDriver driver;
+    public static RemoteWebDriver driver;
 
     @BeforeAll
     public static void setup() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("window-size=1400,800");
+        options.addArguments("disable-gpu");
+
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
@@ -23,7 +34,7 @@ abstract public class TestBase {
     }
 
     @AfterEach
-    public void tearDown() {
+    public void tearDown(){
         driver.close();
         driver.quit();
     }
